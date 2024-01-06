c3637324c747cd2a5ab5f3cb53502d7a864fac72
Martin Stuwe <MartinStuwe@ProtonMail.com>
2024-01-06 07:46:12 +0100

    init commit

---
+++ main.s
@@ -0,0 +1,104 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Enter height of box: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"Enter length of box: "
+.LC3:
+	.string	"Enter width of box: "
+.LC4:
+	.string	"Volume (cubic inches): %d\n"
+	.align 8
+.LC5:
+	.string	"Dimensional weight (pounds): %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_of			fset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-28(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-20(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-28(%rbp), %edx
+	movl	-24(%rbp), %eax
+	imull	%eax, %edx
+	movl	-20(%rbp), %eax
+	imull	%edx, %eax
+	movl	%eax, -16(%rbp)
+	movl	-16(%rbp), %eax
+	addl	$165, %eax
+	movslq	%eax, %rdx
+	imulq	$827945503, %rdx, %rdx
+	shrq	$32, %rdx
+	movl	%edx, %ecx
+	sarl	$5, %ecx
+	cltd
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	movl	%eax, -12(%rbp)
+	movl	-16(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC5(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 13.2.1 20230801"
+	.section	.note.GNU-stack,"",@progbits
